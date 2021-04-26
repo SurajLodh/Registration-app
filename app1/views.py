0ec628fdd9b320d75807c276c6fb64e2b729fd0a
SurajLodh <surajlodh10962@gmail.com>
2021-04-27 00:57:10 +0530

    first start

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def Register(request):
+    
+
+
+    
+    return render(request, 'index.html')
